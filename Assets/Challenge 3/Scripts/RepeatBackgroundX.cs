08e1d885ebe1e6d22e6a71327189398d014c0fb2
Gabrielle Pangilinan <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2022-12-15 16:55:03 +0800

    Group 5-Challenge 3

---
+++ Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackgroundX : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+
+    void Start()
+    {
+        startPos = transform.position; // Establish the default starting position 
+        //Challenge 5: Change size.y to size.x
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2; // Set repeat width to half of the background
+    }
+
+    void Update()
+    {
+        // If background moves left by its repeat width, move it back to start position
+        //Challenge 5: Change startPos.y to startPos.x
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+
+ 
+}
+
+
